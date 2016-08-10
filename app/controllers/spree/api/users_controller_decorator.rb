ce1d1a65b106f96fc65633e782726a9cf157b1fa
Michael Dudzik <mdudzik@gmail.com>
2016-08-10 11:18:10 -0700

    fix syntax error

--- app/controllers/spree/api/users_controller_decorator.rb
+++ app/controllers/spree/api/users_controller_decorator.rb
@@ -2,40 +2,40 @@ module Spree
   module Api
     module V1
 
-    UsersController.class_eval do
+      UsersController.class_eval do
 
-      before_action :authenticate_user, :except => [:sign_up, :sign_in]
+        before_action :authenticate_user, :except => [:sign_up, :sign_in]
 
-      def sign_up
+        def sign_up
 
-        @user = Spree::User.find_by_email(params[:user][:email])
+          @user = Spree::User.find_by_email(params[:user][:email])
 
-        if @user.present?
-          render "spree/api/users/user_exists", :status => 401 and return
+          if @user.present?
+            render "spree/api/users/user_exists", :status => 401 and return
+          end
+
+          @user = Spree::User.new(user_params)
+          if !@user.save
+            unauthorized
+            return
+          end
+          @user.generate_spree_api_key!
         end
 
-        @user = Spree::User.new(user_params)
-        if !@user.save
-          unauthorized
-          return
+        def sign_in
+          @user = Spree::User.find_by_email(params[:user][:email])
+          if !@user.present? || !@user.valid_password?(params[:user][:password])
+            unauthorized
+            return
+          end
+          @user.generate_spree_api_key! if @user.spree_api_key.blank?
         end
-        @user.generate_spree_api_key!
-      end
 
-      def sign_in
-        @user = Spree::User.find_by_email(params[:user][:email])
-        if !@user.present? || !@user.valid_password?(params[:user][:password])
-          unauthorized
-          return
+  
+        def user_params
+          params.require(:user).permit(:email, :password, :password_confirmation)
         end
-        @user.generate_spree_api_key! if @user.spree_api_key.blank?
       end
-
-
-      def user_params
-        params.require(:user).permit(:email, :password, :password_confirmation)
-      end
-
     end
   end
 end
